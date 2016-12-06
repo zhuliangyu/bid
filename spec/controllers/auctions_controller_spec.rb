7b2b4c1815c7f612ab188aa6e8df0d58ff3d7e57
Ricky Zhu <ricky@enbut.com>
2016-12-06 09:43:45 -0800

    add rspec back

--- spec/controllers/auctions_controller_spec.rb
+++ spec/controllers/auctions_controller_spec.rb
@@ -1,40 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe AuctionsController, type: :controller do
+  describe "GET #new" do
+    context ' when user log in' do
+      user = FactoryGirl.create(:user)
+      it "redirect to new template" do
+        get(:new, session: {'user_id' => user.id})
+        expect(response).to render_template(:new) # wraps assert_redirected_to
+      end
+    end
 
-  # describe "GET #new" do
-  #   context ' when user log in' do
-  #     user = FactoryGirl.create(:user)
-  #     it "redirect to new template" do
-  #       get(:new, session: {'user_id' => user.id})
-  #       expect(response).to render_template(:new) # wraps assert_redirected_to
-  #     end
-  #   end
-  #
-  #   context ' when user not log in' do
-  #     it "redirect to home page" do
-  #       get(:new)
-  #       expect(response).to redirect_to(root_path) # wraps assert_redirected_to
-  #     end
-  #   end
-  # end
+    context ' when user not log in' do
+      it "redirect to home page" do
+        get(:new)
+        expect(response).to redirect_to(root_path) # wraps assert_redirected_to
+      end
+    end
+  end
 
   describe "POST #create" do
-    # context ' when user log in' do
-    #   user = FactoryGirl.create(:user)
-    #   it "should create a new auction" do
-    #     before_count=Auction.count #0
-    #
-    #     post(:create, session: {'user_id' => user.id}, params: {auction: FactoryGirl.attributes_for(:auction)})
-    #
-    #     after_count=Auction.count #1
-    #
-    #     expect(before_count+1).to eq(after_count)
-    #
-    #   end
-    #
-    #
-    # end
+    context ' when user log in' do
+      user = FactoryGirl.create(:user)
+      it "should create a new auction" do
+        before_count=Auction.count #0
+
+        post(:create, session: {'user_id' => user.id}, params: {auction: FactoryGirl.attributes_for(:auction)})
+
+        after_count=Auction.count #1
+
+        expect(before_count+1).to eq(after_count)
+
+      end
+
+
+    end
 
     context ' when user not log in' do
 
